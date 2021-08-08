d4737d6356b0df3fd0f4dadad9a79ddb5e21be83
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-08-08 16:17:39 +0400

    first test case

--- employee-app-starter/src/main/java/com/learnwebclient/dto/Employee.java
+++ employee-app-starter/src/main/java/com/learnwebclient/dto/Employee.java
@@ -1,4 +1,19 @@
 package com.learnwebclient.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+
+    private int empId;
+    private int age;
+    private String firstName;
+    private String gender;
+    private String lastName;
+    private String role;
+
 }
--- employee-app-starter/src/main/java/com/learnwebclient/service/EmployeeRestClient.java
+++ employee-app-starter/src/main/java/com/learnwebclient/service/EmployeeRestClient.java
@@ -1,4 +1,27 @@
 package com.learnwebclient.service;
 
+import com.learnwebclient.dto.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+import static com.learnwebclient.constants.EmployeeConstants.GET_ALL_EMP_V1;
+
 public class EmployeeRestClient {
+
+    private WebClient webClient;
+
+    public EmployeeRestClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public List<Employee> getAllEmp() {
+        return webClient.get().uri(GET_ALL_EMP_V1)
+                .retrieve()
+                .bodyToFlux(Employee.class)
+                .collectList()
+                .block();
+    }
+
 }
