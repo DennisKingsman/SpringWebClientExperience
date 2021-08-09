64c947ee5eb0e6c566b967298e6998a13c687d9e
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-08-10 00:07:15 +0400

    try catch update

--- employee-app-starter/src/main/java/com/learnwebclient/service/EmployeeRestClient.java
+++ employee-app-starter/src/main/java/com/learnwebclient/service/EmployeeRestClient.java
@@ -1,13 +1,16 @@
 package com.learnwebclient.service;
 
 import com.learnwebclient.dto.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
 import static com.learnwebclient.constants.EmployeeConstants.GET_ALL_EMP_V1;
 import static com.learnwebclient.constants.EmployeeConstants.GET_EMP_BY_ID_V1;
 
+@Slf4j
 public class EmployeeRestClient {
 
     private WebClient webClient;
@@ -25,10 +28,24 @@ public class EmployeeRestClient {
     }
 
     public Employee getEmpById(int empId) {
-        return webClient.get().uri(GET_EMP_BY_ID_V1, empId)
-                .retrieve()
-                .bodyToMono(Employee.class)
-                .block();
+        try {
+            return webClient.get().uri(GET_EMP_BY_ID_V1, empId)
+                    .retrieve()
+                    .bodyToMono(Employee.class)
+                    .block();
+        }catch (WebClientResponseException ex) {
+            log.error(
+                    "Response code is {} and the response body is {}",
+                    ex.getRawStatusCode(),
+                    ex.getResponseBodyAsString()
+            );
+            log.error(ex.getMessage());
+            log.error("StackTrace: {}", ex);
+            throw ex;
+        } catch (Exception ex) {
+            log.error("StackTrace: {}", ex);
+            throw ex;
+        }
     }
 
 }
